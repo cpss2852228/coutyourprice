84bd886120ae764782e7f170a90271715ac86bf8
cpss2852228 <cpss2852228@gmail.com>
2024-05-21 14:54:11 +0800

    version9: 新增折扣

--- countprice/app.js
+++ countprice/app.js
@@ -5,17 +5,55 @@ let iconCart = document.querySelector('.icon-cart');
 let iconCartSpan = document.querySelector('.icon-cart span');
 let body = document.querySelector('body');
 let closeCart = document.querySelector('.close');
+let orig = document.querySelector('.orig');
+let dc95 = document.querySelector('.dc95');
+let dc7 = document.querySelector('.dc7');
+let dc65 = document.querySelector('.dc65');
+let dc62 = document.querySelector('.dc62');
+let dc57 = document.querySelector('.dc57');
+let dc52 = document.querySelector('.dc52 ');
 let products = [];
 let cart = [];
-
-
+var discount=1;
+var copyPrice=0;
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
 })
 closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
 })
-
+orig.addEventListener('click', () => {
+    discount=1;
+    clickHandler(discount);
+})
+dc95.addEventListener('click', () => {
+    discount=0.95;
+    clickHandler(discount);
+})
+dc7.addEventListener('click', () => {
+    discount=0.7;
+    clickHandler(discount);
+})
+dc65.addEventListener('click', () => {
+    discount=0.65;
+    clickHandler(discount);
+})
+dc62.addEventListener('click', () => {
+    discount=0.62;
+    clickHandler(discount);
+})
+dc57.addEventListener('click', () => {
+    discount=0.57;
+    clickHandler(discount);
+})
+dc52.addEventListener('click', () => {
+    discount=0.52;
+    clickHandler(discount);
+})
+var clickHandler = function(discount){
+    listCartprice.innerText = "總額為：" + Math.ceil(copyPrice*discount);
+    console.log(copyPrice,"*",discount,"=",copyPrice*discount);
+  };
     const addDataToHTML = () => {
     // remove datas default from HTML
 
@@ -30,7 +68,7 @@ closeCart.addEventListener('click', () => {
                 `<img src="${product.image}" alt="">
                 <h2>${product.name}</h2>
                 <div class="price">$${product.price}</div>
-                <button class="addCart">Add To Cart</button>`;
+                <button class="addCart">加入購物車</button>`;
                 listProductHTML.appendChild(newProduct);
             });
         }
@@ -95,7 +133,10 @@ const addCartToHTML = () => {
             `;
         })
     }
-    listCartprice.innerText = "總額為：" + totalPrice;
+    copyPrice=totalPrice;
+    pdc=totalPrice*discount;
+    console.log(pdc);
+    listCartprice.innerText = "總額為：" + Math.ceil(pdc);
     iconCartSpan.innerText = totalQuantity;
 }
 
@@ -130,7 +171,7 @@ const changeQuantityCart = (product_id, type) => {
         }
     }
     addCartToHTML();
-    addCartToMemory();
+    //addCartToMemory();
 }
 
 const initApp = () => {
